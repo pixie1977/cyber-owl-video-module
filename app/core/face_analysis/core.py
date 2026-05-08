b50f5928513221e06abc4639e78843b532bd49bd
Pixie <eugenmac56@gmail.com>
2026-05-09 02:17:47 +0300

    - доработка логики

--- app/core/face_analysis/core.py
+++ app/core/face_analysis/core.py
@@ -7,12 +7,12 @@ logger = logging.getLogger(__name__)
 def get_pipeline():
     try:
         logger.info("Попытка загрузить FaceAnalysis на GPU (ctx_id=0)")
-        app = FaceAnalysis(name="buffalo_s", ctx_id=0, det_size=(640, 640))
+        app = FaceAnalysis(name="buffalo_m", ctx_id=0, det_size=(640, 640))
         app.prepare(ctx_id=0, det_size=(640, 640))
         logger.info("FaceAnalysis успешно загружена на GPU")
     except Exception as e:
         logger.warning(f"Ошибка при загрузке на GPU: {e}. Переключаемся на CPU.")
-        app = FaceAnalysis(name="buffalo_s", ctx_id=-1, det_size=(640, 640))
+        app = FaceAnalysis(name="buffalo_m", ctx_id=-1, det_size=(640, 640))
         app.prepare(ctx_id=-1, det_size=(640, 640))
         logger.info("FaceAnalysis загружена на CPU")
     return app
